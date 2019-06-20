d500db55bc2da9e3c1afe6c785582527bea94cb4
tanziwen <805403077@qq.com>
2019-06-20 16:13:01 +0800

    third

--- devops/views.py
+++ devops/views.py
@@ -9,20 +9,20 @@ import json
 
 def user_info(request):
     # 调用request.META（元数据方法）
-    print(request.META)
-    # 获取远程主机的ip地址
+    # print(request.META)
+    # 获取用户端的ip地址
     ip_addr = request.META['REMOTE_ADDR']
-    # 获取远程主机的浏览器头部信息
+    # 获取用户端的浏览器信息
     user_ua = request.META['HTTP_USER_AGENT']
-
+    print(len(user_ua),user_ua)
     user_obj = UserIPInfo.objects.filter(ip = ip_addr)
-
-    if len(user_obj)== 0:
+    if len(user_obj)==0:
         print("empty")
         res = UserIPInfo.objects.create(ip = ip_addr)
         ip_addr_id = res.id
     else:
-        ip_addr_id = user_obj[0]
+        ip_addr_id = user_obj[0].id
+        print(ip_addr_id)
 
     BrowerInfo.objects.create(useragent=user_ua,userip_id=ip_addr_id)
     result = {
