58d831fe268e32736476ef9cfd486fc2bb3232a3
tanziwen <805403077@qq.com>
2019-06-30 17:07:15 +0800

    6-30 lastest-test2

--- devops/util/tools.py
+++ devops/util/tools.py
@@ -3,4 +3,20 @@ import time
 
 class sendmail():
     def __init__(self,receive_addr,sub_info,content_info):
-        sub_data = time.strftime("")
+        sub_data = time.strftime("%Y-%m-%d-%H:%M:%S",time.localtime())
+        self.receive_addr = receive_addr
+        self.sub_info = sub_info
+        self.content_info = content_info
+
+    def send(self):
+        try:
+            send_mail(subject = self.sub_info,
+                      message =self.content_info,
+                      from_email = '805403077@qq.com',
+                      recipient_list = self.receive_addr,
+                      fail_sliently = False,
+                      )
+            return True
+        except Exception as e:
+            print(e)
+            return False
